3b40522f07fd74f72db5ae63ab98e2c48b66f5cf
Borothana <samnang.borathana@gmail.com>
2018-06-25 11:14:38 -0500

    V1

---
+++ BasicAuthentication/App_Start/WebApiConfig.cs
@@ -0,0 +1,28 @@
+﻿using BasicAuthentication.Filter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace BasicAuthentication
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+            config.Filters.Add(new BasicAuthenticationFilter());
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            //config.Filters.Add(BasicAuthenticationFilter);
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ BasicAuthentication/Filter/BasicAuthenticationFilter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Principal;
+using System.Text;
+using System.Threading;
+using System.Web;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace BasicAuthentication.Filter
+{
+    public class BasicAuthenticationFilter:AuthorizationFilterAttribute
+    {
+        public override void OnAuthorization(HttpActionContext actionContext)
+        {
+            //base.OnAuthorization(actionContext);
+            if(actionContext.Request.Headers.Authorization == null)
+            {
+                actionContext.Response = actionContext.Request.CreateResponse(HttpStatusCode.Unauthorized);
+                //actionContext.Response.Content =new HttpResponseMessage( HttpStatusCode.Unauthorized "123456789";
+            }
+            else
+            {
+                string authenticationToken = actionContext.Request.Headers.Authorization.Parameter;
+                if (authenticationToken != null)
+                {
+
+                    var auth = Encoding.UTF8.GetString(Convert.FromBase64String(authenticationToken));
+                    string[] identity = auth.Split(':');
+                    var userName = identity[0];
+                    var password = identity[1];
+
+                    if (userName == "borothana.doul@gmail.com" && password == "12345678")
+                    {
+                        Thread.CurrentPrincipal = new GenericPrincipal(new GenericIdentity(userName), new string[] { "admin" });
+                        return;
+                    }                    
+                }
+                actionContext.Response = actionContext.Request.CreateResponse(HttpStatusCode.Unauthorized);
+            }
+        }
+    }
+}
